1c304466ad563cf3b69c94dd5531bdf9a0b15b89
jungbin <zackal0812@gmail.com>
2024-06-05 21:07:07 +0900

    tst

---
+++ Hashtable/Hashtable/HongsClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hahtable
+{
+    internal class HongsClass
+    {
+       public string Name { get; set; }
+       public int intCount { get; set; }
+        
+    }
+}
---
+++ Hashtable/Hashtable/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections;
+using static System.Console;
+namespace Hahtable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable hashtable = new Hashtable();
+
+            hashtable.Add("Data1", new HongsClass() { Name = "홍진현1", intCount = 1 });
+            hashtable.Add("Data2", new HongsClass() { Name = "홍진현2", intCount = 2 });
+
+            if (hashtable.ContainsKey("Data1").Equals(true))
+            {
+                HongsClass temp = hashtable["Data1"] as HongsClass;
+                Console.WriteLine(temp.Name);
+            }
+
+            foreach (string NowKey in hashtable.Keys)
+            {
+                HongsClass temp = hashtable[NowKey] as HongsClass;
+                Console.WriteLine(temp.Name);
+            
+            }
+        }
+    }
+}
