92be1521994df00bfe5de20d840774106a3a9b1d
jungbin <zackal0812@gmail.com>
2024-06-05 21:15:38 +0900

    tst

---
+++ Indxer/Indxer/MyList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indxer
+{
+    internal class MyList
+    {
+        private int[] array;
+
+        public MyList()
+        {
+            array = new int[3];
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+
+            set
+            {
+                if (index >= array.Length)
+                {
+                    Array.Resize<int>(ref array, index+1);
+                    Console.WriteLine($"Array Resized : {array.Length}");
+                }
+
+                array[index] = value;
+            }
+        }
+
+        public int Length
+        {
+            get { return array.Length;}
+        }
+    }
+}
---
+++ Indxer/Indxer/Program.cs
@@ -0,0 +1,18 @@
+﻿using Indxer;
+
+namespace Indexer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList list = new MyList();
+            for (int i = 0; i < 5; i++)
+                list[i] = i;
+
+            for (int i = 0; i<list.Length; i++)
+                Console.WriteLine(list[i]);
+            
+        }
+    }
+}
